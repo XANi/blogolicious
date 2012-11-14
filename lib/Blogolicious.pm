df156181cde86db81cbe3fcdad2a94d6e06d3e54
XANi <xani666@gmail.com>
2012-11-14 01:36:39 +0100

    testing

--- lib/Blogolicious.pm
+++ lib/Blogolicious.pm
@@ -1,18 +1,41 @@
 package Blogolicious;
 use Mojo::Base 'Mojolicious';
 
+use YAML::XS;
+use File::Slurp qw(read_file);
+
+
 # This method will run once at server start
 sub startup {
-  my $self = shift;
+    my $self = shift;
+    my $cfg = read_file('cfg/config.yaml');
+    $cfg = Load($cfg) or die;
+    print "\n----- started: " . scalar localtime(time()) . "----\n";
+    print "Config:\n" . Dump($cfg);
+    $self->plugin(
+        tt_renderer => {
+            template_options => {
+                INCLUDE_PATH => $cfg->{'repo_dir'},
+                COMPILE_DIR => $cfg->{'tmp_dir'} . '/tt_cache',
+                COMPILE_EXT => '.ttc',
+                CACHE_SIZE =>0, # 0 means no cache
+            }
+        }
+    );
 
-  # Documentation browser under "/perldoc"
-  $self->plugin('PODRenderer');
+    $self->renderer->default_handler('tt');
+    # Documentation browser under "/perldoc"
+    $self->plugin('PODRenderer');
 
-  # Router
-  my $r = $self->routes;
+    # Router
+    my $r = $self->routes;
 
-  # Normal route to controller
-  $r->get('/')->to('example#welcome');
+    # Normal route to controller
+#    $r->get('/')->to('example#welcome');
+    $r->get(
+       '/' => sub {
+           my $self = shift;
+           $self->render(template=>'index');
+       });
 }
-
 1;
