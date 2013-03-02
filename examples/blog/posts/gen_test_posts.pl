751468986a28a78a92e26b441280fc1a28414a91
Mariusz Gronczewski <xani666@gmail.com>
2013-03-02 17:57:37 +0100

    add posts for tests

--- examples/blog/posts/gen_test_posts.pl
+++ examples/blog/posts/gen_test_posts.pl
@@ -7,6 +7,7 @@ use YAML::XS;
 use List::Util qw/shuffle/;
 use Digest::SHA qw(sha1_hex);
 use Data::Dumper;
+srand(0); #predictable for tests
 my $text = q{**Lorem Ipsum** is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the _1500s_, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the _1960s_ with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like **Aldus PageMaker** including versions of Lorem Ipsum.};
 
 my @tags = (
