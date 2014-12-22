899d3bba5f125920370d9fe86346284ef4a8c5aa
Mariusz Gronczewski <xani666@gmail.com>
2014-12-22 23:07:40 +0100

    deps

--- pkgdeps.sh
+++ pkgdeps.sh
@@ -1,3 +1,3 @@
 # deps required on barebone system to build
-apt-get install -y build-essential libxml-parser-perl carton
+apt-get install -y build-essential libxml-parser-perl carton libexpat1-dev libxml2-dev zlib1g-dev bogofilter
 
