ae57df9a9794ca8269da6ea6fcea67e1fe8655ef
Mariusz Gronczewski <xani666@gmail.com>
2015-05-05 04:19:00 +0200

    add bg script

--- run-prod-bg.sh
+++ run-prod-bg.sh
@@ -2,4 +2,4 @@
 ROOT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 echo "moving to script's root dir at [$ROOT_DIR]"
 cd "$ROOT_DIR"
-exec carton exec --  hypnotoad -f script/blogolicious $@
+exec carton exec --  hypnotoad script/blogolicious $@
